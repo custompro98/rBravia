995443827b02e9d0ec627991f15fef5798137f57
Mitch Joa <mitchjoa@gmail.com>
2016-04-24 22:55:14 -0400

    Attempt at authorization

--- test/test.rb
+++ test/test.rb
@@ -1,3 +1,10 @@
 require 'rBravia'
 
+ip = "192.168.10.156"
+psk = "1203"
+nick = "rubyClient"
+volDown = "AAAAAQAAAAEAAAATAw=="
+
+RBravia.init(ip, psk, nick)
 RBravia.register
+RBravia.sendCommand(volDown)
